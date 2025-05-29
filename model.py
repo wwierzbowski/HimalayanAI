9097b9dc4dd8b67545cf9d41db86ef2dbccba00d
will-dabeast09 <wwierzbow@gmail.com>
2025-05-29 14:06:13 -0700

    add neural network class

---
+++ model.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+class NN(nn.Module):
+    def __init__(self):
+        super(NN, self).__init__()
+        self.fc1 = nn.Linear(18, 6)
+        self.fc2 = nn.Linear(6, 6)
+        self.fc3 = nn.Linear(6, 1)
+
+        self.relu = nn.ReLU()
+    
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+
+        x = self.fc2(x)
+        x = self.relu(x)
+
+        x = self.fc3(x)
+
+        return x
